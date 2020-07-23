8964b3e36ef4720d8c637c0208011eb156433df3
Siva <gnusiva@gmail.com>
2020-07-24 01:09:05 +0530

    step3 writing to database from json file by spark

--- 02filterData/filter.py
+++ 02filterData/filter.py
@@ -5,13 +5,17 @@ with open('../01fetchAPI/output.json') as f:
     data = json.load(f)
 
 currency_list = ['EUR', 'USD', 'JPY', 'CAD', 'GBP', 'NZD','INR']
-filtered_data = {}
+filtered_data = []
 # pprint(data)
 for date in data['rates']:
-    filtered_data[date] = {}
     for currency, currency_value in data['rates'][date].items():
         if currency in currency_list:
-            filtered_data[date][currency] = currency_value
+            # filtered_data[date][currency] = currency_value
+            dictObj = {}
+            dictObj['date'] = date
+            dictObj['currencyId'] = currency_list.index(currency)
+            dictObj['value'] = currency_value
+            filtered_data.append(dictObj)
 
 with open('filtered_data.json', 'w') as f:
     json.dump(filtered_data, f, indent=4)
--- 03spark/spark.py
+++ 03spark/spark.py
@@ -1,18 +1,14 @@
-# from pyspark import SparkContext
-# sc =SparkContext.getOrCreate()
 
-# from pyspark.sql import SparkSession
-# sess = SparkSession(sc)
-
-# jdf = sess.read.json("../02filterData/filtered_data.json")
-# jdf.withColumn('Date',jdf).show()
 
 from pyspark.context import SparkContext
 from pyspark.sql.session import SparkSession
 sc = SparkContext('local')
 spark = SparkSession(sc)
 
-df = spark.read.json("../02filterData/filtered_data.json", multiLine=True)
+path = '../02filterData/filtered_data.json'
+df = spark.read.json(path, multiLine=True)
 
-df.printSchema()
+df.write.format("jdbc").options(url ="jdbc:sqlite:test.db", driver="org.sqlite.JDBC", dbtable="DailyExchange").save()
+# df.printSchema()
+# df.show()
 
