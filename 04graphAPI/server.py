9679a6c517f1eb0a2749ecffd908d7da1febeb83
Siva <gnusiva@gmail.com>
2020-07-24 02:33:50 +0530

    server and chart working fine

--- 04graphAPI/server.py
+++ 04graphAPI/server.py
@@ -1,10 +1,33 @@
 from flask import Flask
+from flask import request
+import sqlite3
+
+from flask import jsonify
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
+@app.route('/exchangeData')
+def exchangeData():
+    currency = request.args.get('currency')
+    return queryDatabase(currency)
+
+def queryDatabase(currency):
+    conn = sqlite3.connect('../03spark/test.db')
+    c = conn.cursor()
+    result = []
+    for row in c.execute("SELECT * FROM DailyExchange  WHERE currencyId='6' ORDER BY date LIMIT 100"):
+        date = row[1]
+        value = row[2]
+        result.append([date, value])
+    conn.close()
+    resp = jsonify(result)
+    resp.headers.add('Access-Control-Allow-Origin', '*')
+    return resp
 
 if __name__ == '__main__':
     app.run()
